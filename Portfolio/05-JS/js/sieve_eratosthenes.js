d12d82ee4e8160b70612149b5c277efc6290ccec
Santiago Macias <santiago.macias.fernandez@gmail.com>
2025-10-05 11:21:52 -0600

    prime, next prime, sieve done

--- Portfolio/05-JS/js/sieve_eratosthenes.js
+++ Portfolio/05-JS/js/sieve_eratosthenes.js
@@ -8,14 +8,32 @@
 var sieve = function (n) {
   "use strict";
 
-  var array = [],
-    primes = [],
-    i,
-    j;
-
+ 
   // TODO: Implement the sieve of eratosthenes algorithm to find all the prime numbers under the given number.
 
-  return primes;
-};
+  let isPrime = new Array(n + 1).fill(true);
+  isPrime[0] = isPrime[1] = false;
 
+  for (let i = 2; i * i <= n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  let primes = [];
+  for (let i = 2; i <= n; i++) {
+    if (isPrime[i]) primes.push(i);
+  }
+  return primes;
+}
+
+window.onload = function () {
+  document.getElementById('btn').onclick = function () {
+    let val = parseInt(document.getElementById('num').value);
+    let primes = sieve(val);
+    document.getElementById('primes').innerHTML = primes.join(', ');
+  };
+};
 console.log(sieve(1000000));
